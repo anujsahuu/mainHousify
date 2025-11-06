25f92f4ee56e3700f33e4b30d307bdf5fe6576df
Anuj <9.20anujsahu@gmail.com>
2025-11-07 02:41:26 +0530

    `Update init/index.js to use environment variables for MongoDB URL`

--- init/index.js
+++ init/index.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+require("dotenv").config();
+const dotenv = require("dotenv");
 
-const MONGO_URl = "mongodb://127.0.0.1:27017/housify";
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config(); // Load variables from .env for the script
+// }
+//const MONGO_URl = "mongodb://127.0.0.1:27017/housify";
+const db_url = process.env.ATLAS_DB_URL;
 
 connectToDatabase().then(() => {
     console.log("Connected to MongoDB");
@@ -11,7 +17,7 @@ connectToDatabase().then(() => {
 });
 
 async function connectToDatabase() {
-        await mongoose.connect(MONGO_URl);
+        await mongoose.connect(db_url);
 }
 
 const initDB = async () => {
